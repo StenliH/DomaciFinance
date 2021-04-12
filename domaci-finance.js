e044b28cacbb7ae874e897343fa86001fbf95407
Martin Hlaváč <martin.hlavac@pds.eu>
2021-04-12 16:19:28 +0200

    - tabulka Zůstatky se nyní vytváří pomocí JS - sloupce a řádky v tabulkách jsou nadepsány pomocí objektů v polích

--- domaci-finance.js
+++ domaci-finance.js
@@ -1,12 +1,37 @@
-﻿function createSelectYear() {
-    // // get parent div
-    // var parentDiv = document.getElementById("select-year-container");
+﻿/**
+ * Názvy sloupců z tabulky Zůstatky s funkcemi pro výpočet hodnot.
+ */
+var zustatkyTableColumnsHeader = [
+    {label: 'Běžná spotřeba', value: getBeznaSpotreba},
+    // {label: 'Rezerva', value: ??},
+    // {label: 'Martin', value: ??},
+    // {label: 'Káťa', value: ??}
+];
 
-    // // create select and add it to parent
-    // var selectYear = document.createElement("select");
-    // selectYear.id = "select-year";
-    // parentDiv.appendChild(selectYear);
-    
+// TODO - odkomentovat a upravit
+/**
+ * Názvy řádků z tabulky Cashflow s funkcemi pro výpočet hodnot.
+ */
+var cashflowTableRowsHeader = [
+    {label: 'Běžná spotřeba', value: getBeznaSpotreba},
+    // {label: 'Rezerva', value: ??},
+    // {label: 'Martin', value: ??},
+    // {label: 'Káťa', value: ??},
+    // {label: 'Celkově', value: ??},
+];
+
+// TODO - odkomentovat a upravit
+/**
+ * Názvy řádků z tabulky Výdaje s funkcemi pro výpočet hodnot.
+ */
+var vydajeTableRowsHeader = [
+    // {label: 'Jídlo a pití', value: ??},
+    // {label: 'Nákupy', value: ??},
+    // {label: 'Charita a dárky', value: ??}
+];
+
+// pro select s roky vytvoří options (od roku 2020 po aktuální) a vybere letošek
+function createOptionsForSelectYear() {
     // add options and set actual
     var selectYear = document.getElementById("years");
 
@@ -20,10 +45,43 @@
     selectYear.value = actualYear;
 }
 
+/**
+ * vytvoří tabulku v příslušném containeru s danou vnitřní strukturou
+ * @param {*} containerId 
+ * @param {*} tableStructure 
+ */
+function createTable(containerId, tableStructure) {
+    let table = document.createElement("table");
+    tableStructure(table);
 
-// <select name="years" id="years" onchange="main()" style="font-size: 1.5em;">
-// <option value=2020>2020</option>
-// <option value="2021">2021</option>
-// <option value="2022">2022</option>
-// <option value="2023">2023</option>
-// </select>
+    let parent = document.getElementById(containerId);
+    parent.appendChild(table);
+}
+
+
+function zustatkyTableStructure(table) {
+    let headersRow = document.createElement("tr");
+    let valuesRow = document.createElement("tr");
+
+    zustatkyTableColumnsHeader.forEach(e => {
+        let tdHeader = document.createElement("th");
+        tdHeader.innerText = e.label;
+        headersRow.appendChild(tdHeader);
+
+        let tdValue = document.createElement("td");
+        tdValue.innerText = e.value("ahoj");
+        valuesRow.appendChild(tdValue);
+    });
+    table.appendChild(headersRow);
+    table.appendChild(valuesRow);
+}
+
+/**
+ * Z daných transakcí vypočte běžnou spotřebu.
+ * @param { array } transactions Pole všech relevantních transakcí.
+ * @returns {number} běžná spotřeba
+ */
+function getBeznaSpotreba(transactions) {
+    // TODO - upravit tělo metody dle reality
+    return 50;
+}
