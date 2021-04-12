ea7de3389261933a001805e01703e59e7de02120
Martin Hlaváč <martin.hlavac@pds.eu>
2021-04-12 12:04:00 +0200

    file input a select years

--- domaci-finance.js
+++ domaci-finance.js
@@ -1,3 +1,29 @@
-﻿function ahoj() {
-    alert("Hooray!");
+﻿function createSelectYear() {
+    // // get parent div
+    // var parentDiv = document.getElementById("select-year-container");
+
+    // // create select and add it to parent
+    // var selectYear = document.createElement("select");
+    // selectYear.id = "select-year";
+    // parentDiv.appendChild(selectYear);
+    
+    // add options and set actual
+    var selectYear = document.getElementById("years");
+
+    var actualYear = new Date().getFullYear()
+    for (i = 2020; i < actualYear + 1; i++) {
+        var option = document.createElement("option");
+        option.value = i;
+        option.text = i;
+        selectYear.appendChild(option);
+    }
+    selectYear.value = actualYear;
 }
+
+
+// <select name="years" id="years" onchange="main()" style="font-size: 1.5em;">
+// <option value=2020>2020</option>
+// <option value="2021">2021</option>
+// <option value="2022">2022</option>
+// <option value="2023">2023</option>
+// </select>
